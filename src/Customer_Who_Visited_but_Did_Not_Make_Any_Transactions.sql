60353bdc81c702912424b3f0720c14ecfeffd70b
Aishwarya-University-of-waterloo <aswagh@uwaterloo.ca>
2024-05-08 09:00:34 -0400

    Easy problems

---
+++ src/Customer_Who_Visited_but_Did_Not_Make_Any_Transactions.sql
@@ -0,0 +1,55 @@
+-- 1581. Customer Who Visited but Did Not Make Any Transactions
+-- SOURCE: https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- PROBLEM:
+-- Table: Visits
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | visit_id    | int     |
+-- | customer_id | int     |
+-- +-------------+---------+
+-- visit_id is the column with unique values for this table.
+-- This table contains information about the customers who visited the mall.
+
+-- Table: Transactions
+
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | transaction_id | int     |
+-- | visit_id       | int     |
+-- | amount         | int     |
+-- +----------------+---------+
+-- transaction_id is column with unique values for this table.
+-- This table contains information about the transactions made during the visit_id.
+
+
+-- Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+-- Return the result table sorted in any order.
+
+-- TABLE SCHEMA:
+Create table If Not Exists Visits(visit_id int, customer_id int)
+Create table If Not Exists Transactions(transaction_id int, visit_id int, amount int)
+Truncate table Visits
+insert into Visits (visit_id, customer_id) values ('1', '23')
+insert into Visits (visit_id, customer_id) values ('2', '9')
+insert into Visits (visit_id, customer_id) values ('4', '30')
+insert into Visits (visit_id, customer_id) values ('5', '54')
+insert into Visits (visit_id, customer_id) values ('6', '96')
+insert into Visits (visit_id, customer_id) values ('7', '54')
+insert into Visits (visit_id, customer_id) values ('8', '54')
+Truncate table Transactions
+insert into Transactions (transaction_id, visit_id, amount) values ('2', '5', '310')
+insert into Transactions (transaction_id, visit_id, amount) values ('3', '5', '300')
+insert into Transactions (transaction_id, visit_id, amount) values ('9', '5', '200')
+insert into Transactions (transaction_id, visit_id, amount) values ('12', '1', '910')
+insert into Transactions (transaction_id, visit_id, amount) values ('13', '2', '970')
+
+-- SOLUTION:
+SELECT
+    customer_id, count(*) as count_no_trans
+FROM visits
+WHERE visit_id not in (SELECT distinct visit_id FROM transactions)
+GROUP BY customer_id
