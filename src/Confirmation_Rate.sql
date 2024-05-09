54927271b0dffa19051ad53d471f506566c84cb8
Aishwarya-University-of-waterloo <aswagh@uwaterloo.ca>
2024-05-09 10:16:44 -0400

    Medium problems

---
+++ src/Confirmation_Rate.sql
@@ -0,0 +1,68 @@
+-- 1934. Confirmation Rate
+-- SOURCE: https://leetcode.com/problems/confirmation-rate/?envType=study-plan-v2&envId=top-sql-50
+
+-- PROBLEM:
+-- Table: Signups
+
+-- +----------------+----------+
+-- | Column Name    | Type     |
+-- +----------------+----------+
+-- | user_id        | int      |
+-- | time_stamp     | datetime |
+-- +----------------+----------+
+-- user_id is the column of unique values for this table.
+-- Each row contains information about the signup time for the user with ID user_id.
+
+-- Table: Confirmations
+
+-- +----------------+----------+
+-- | Column Name    | Type     |
+-- +----------------+----------+
+-- | user_id        | int      |
+-- | time_stamp     | datetime |
+-- | action         | ENUM     |
+-- +----------------+----------+
+-- (user_id, time_stamp) is the primary key (combination of columns with unique values) for this table.
+-- user_id is a foreign key (reference column) to the Signups table.
+-- action is an ENUM (category) of the type ('confirmed', 'timeout')
+-- Each row of this table indicates that the user with ID user_id requested a confirmation message at time_stamp and that confirmation message was either confirmed ('confirmed') or expired without confirming ('timeout').
+
+
+-- The confirmation rate of a user is the number of 'confirmed' messages divided by the total number of requested confirmation messages. The confirmation rate of a user that did not request any confirmation messages is 0. Round the confirmation rate to two decimal places.
+-- Write a solution to find the confirmation rate of each user.
+
+-- Return the result table in any order.
+
+-- TABLE SCHEMA:
+Create table If Not Exists Signups (user_id int, time_stamp datetime)
+Create table If Not Exists Confirmations (user_id int, time_stamp datetime, action ENUM('confirmed','timeout'))
+Truncate table Signups
+insert into Signups (user_id, time_stamp) values ('3', '2020-03-21 10:16:13')
+insert into Signups (user_id, time_stamp) values ('7', '2020-01-04 13:57:59')
+insert into Signups (user_id, time_stamp) values ('2', '2020-07-29 23:09:44')
+insert into Signups (user_id, time_stamp) values ('6', '2020-12-09 10:39:37')
+Truncate table Confirmations
+insert into Confirmations (user_id, time_stamp, action) values ('3', '2021-01-06 03:30:46', 'timeout')
+insert into Confirmations (user_id, time_stamp, action) values ('3', '2021-07-14 14:00:00', 'timeout')
+insert into Confirmations (user_id, time_stamp, action) values ('7', '2021-06-12 11:57:29', 'confirmed')
+insert into Confirmations (user_id, time_stamp, action) values ('7', '2021-06-13 12:58:28', 'confirmed')
+insert into Confirmations (user_id, time_stamp, action) values ('7', '2021-06-14 13:59:27', 'confirmed')
+insert into Confirmations (user_id, time_stamp, action) values ('2', '2021-01-22 00:00:00', 'confirmed')
+insert into Confirmations (user_id, time_stamp, action) values ('2', '2021-02-28 23:59:59', 'timeout')
+
+
+-- SOLUTION:
+WITH rate AS(
+    SELECT
+        s.user_id,c.action,
+        sum(case  when action='confirmed' then 1 end) over(partition by c.user_id) as conf,
+        sum(1) over(partition by c.user_id) as total
+    FROM signups s
+    LEFT JOIN confirmations c
+    ON s.user_id = c.user_id
+)
+
+SELECT
+    distinct user_id,
+    coalesce(round((conf/total),2),0) as confirmation_rate
+FROM rate
