883464514c7bfab372ab5ac2e738fb6abb787f9f
Aishwarya-University-of-waterloo <aswagh@uwaterloo.ca>
2024-05-10 09:22:44 -0400

    Medium problems

---
+++ src/Average_Selling_Price.sql
@@ -0,0 +1,57 @@
+-- 1251. Average Selling Price
+-- SOURCE: https://leetcode.com/problems/average-selling-price/description/?envType=study-plan-v2&envId=top-sql-50
+
+
+-- PROBLEM:
+-- Table: Prices
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | product_id    | int     |
+-- | start_date    | date    |
+-- | end_date      | date    |
+-- | price         | int     |
+-- +---------------+---------+
+-- (product_id, start_date, end_date) is the primary key (combination of columns with unique values) for this table.
+-- Each row of this table indicates the price of the product_id in the period from start_date to end_date.
+-- For each product_id there will be no two overlapping periods. That means there will be no two intersecting periods for the same product_id.
+
+-- Table: UnitsSold
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | product_id    | int     |
+-- | purchase_date | date    |
+-- | units         | int     |
+-- +---------------+---------+
+-- This table may contain duplicate rows.
+-- Each row of this table indicates the date, units, and product_id of each product sold.
+
+-- Write a solution to find the average selling price for each product. average_price should be rounded to 2 decimal places.
+-- Return the result table in any order.
+
+-- TABLE SCHEMA:
+Create table If Not Exists Prices (product_id int, start_date date, end_date date, price int)
+Create table If Not Exists UnitsSold (product_id int, purchase_date date, units int)
+Truncate table Prices
+insert into Prices (product_id, start_date, end_date, price) values ('1', '2019-02-17', '2019-02-28', '5')
+insert into Prices (product_id, start_date, end_date, price) values ('1', '2019-03-01', '2019-03-22', '20')
+insert into Prices (product_id, start_date, end_date, price) values ('2', '2019-02-01', '2019-02-20', '15')
+insert into Prices (product_id, start_date, end_date, price) values ('2', '2019-02-21', '2019-03-31', '30')
+Truncate table UnitsSold
+insert into UnitsSold (product_id, purchase_date, units) values ('1', '2019-02-25', '100')
+insert into UnitsSold (product_id, purchase_date, units) values ('1', '2019-03-01', '15')
+insert into UnitsSold (product_id, purchase_date, units) values ('2', '2019-02-10', '200')
+insert into UnitsSold (product_id, purchase_date, units) values ('2', '2019-03-22', '30')
+
+-- SOLUTION:
+SELECT
+    distinct p.product_id,
+    coalesce(round(sum(p.price*u.units) over(partition by p.product_id)/sum(u.units) over(partition by p.product_id),2),0)  as average_price
+FROM prices p
+LEFT JOIN unitssold u
+    ON p.product_id=u.product_id
+WHERE (purchase_date >= start_date and purchase_date<=end_date)
+    OR purchase_date is null
---
+++ src/Not_Boring_Movies.sql
@@ -0,0 +1,38 @@
+-- 620. Not Boring Movies
+-- SOURCE: https://leetcode.com/problems/not-boring-movies/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- PROBLEM:
+-- Table: Cinema
+
+-- +----------------+----------+
+-- | Column Name    | Type     |
+-- +----------------+----------+
+-- | id             | int      |
+-- | movie          | varchar  |
+-- | description    | varchar  |
+-- | rating         | float    |
+-- +----------------+----------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row contains information about the name of a movie, its genre, and its rating.
+-- rating is a 2 decimal places float in the range [0, 10]
+
+-- Write a solution to report the movies with an odd-numbered ID and a description that is not "boring".
+-- Return the result table ordered by rating in descending order.
+
+
+-- TABLE SCHEMA:
+Create table If Not Exists cinema (id int, movie varchar(255), description varchar(255), rating float(2, 1))
+Truncate table cinema
+insert into cinema (id, movie, description, rating) values ('1', 'War', 'great 3D', '8.9')
+insert into cinema (id, movie, description, rating) values ('2', 'Science', 'fiction', '8.5')
+insert into cinema (id, movie, description, rating) values ('3', 'irish', 'boring', '6.2')
+insert into cinema (id, movie, description, rating) values ('4', 'Ice song', 'Fantacy', '8.6')
+insert into cinema (id, movie, description, rating) values ('5', 'House card', 'Interesting', '9.1')
+
+
+-- SOLUTION:
+SELECT
+    id, movie, description, rating
+FROM cinema
+WHERE (id%2)!=0 and description != 'boring'
+ORDER BY rating desc
