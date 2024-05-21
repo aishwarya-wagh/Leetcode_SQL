7f5110410011ff338ef88813a8a481664db21de0
Aishwarya-University-of-waterloo <aswagh@uwaterloo.ca>
2024-05-21 09:46:56 -0400

    Easy problems

---
+++ src/Consecutive_Numbers.sql
@@ -0,0 +1,31 @@
+-- 180. Consecutive Numbers
+-- SOURCE: https://leetcode.com/problems/consecutive-numbers/description/?envType=study-plan-v2&envId=top-sql-50
+
+-- PROBLEM:
+-- Table: Logs
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | num         | varchar |
+-- +-------------+---------+
+-- In SQL, id is the primary key for this table.
+-- id is an autoincrement column.
+
+-- Find all numbers that appear at least three times consecutively.
+-- Return the result table in any order.
+
+
+-- TABLE SCHEMA:
+Create table If Not Exists Logs (id int, num int)
+Truncate table Logs
+insert into Logs (id, num) values ('1', '1')
+insert into Logs (id, num) values ('2', '1')
+insert into Logs (id, num) values ('3', '1')
+insert into Logs (id, num) values ('4', '2')
+insert into Logs (id, num) values ('5', '1')
+insert into Logs (id, num) values ('6', '2')
+insert into Logs (id, num) values ('7', '2')
+
+-- SOLUTION:
---
+++ src/Product_Price_at_a_Given_Date.sql
@@ -0,0 +1,32 @@
+-- 1164. Product Price at a Given Date
+-- SOURCE: https://leetcode.com/problems/product-price-at-a-given-date/?envType=study-plan-v2&envId=top-sql-50
+
+-- PROBLEM:
+-- Table: Products
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | product_id    | int     |
+-- | new_price     | int     |
+-- | change_date   | date    |
+-- +---------------+---------+
+-- (product_id, change_date) is the primary key (combination of columns with unique values) of this table.
+-- Each row of this table indicates that the price of some product was changed to a new price at some date.
+
+-- Write a solution to find the prices of all products on 2019-08-16. Assume the price of all products before any change is 10.
+-- Return the result table in any order.
+
+
+-- TABLE SCHEMA:
+Create table If Not Exists Products (product_id int, new_price int, change_date date)
+Truncate table Products
+insert into Products (product_id, new_price, change_date) values ('1', '20', '2019-08-14')
+insert into Products (product_id, new_price, change_date) values ('2', '50', '2019-08-14')
+insert into Products (product_id, new_price, change_date) values ('1', '30', '2019-08-15')
+insert into Products (product_id, new_price, change_date) values ('1', '35', '2019-08-16')
+insert into Products (product_id, new_price, change_date) values ('2', '65', '2019-08-17')
+insert into Products (product_id, new_price, change_date) values ('3', '20', '2019-08-18')
+
+
+-- SOLUTION:
