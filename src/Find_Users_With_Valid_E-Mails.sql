ebdf62a9a81e22ca8f0bb30f0c8e693e78d0162c
Aishwarya-University-of-waterloo <aswagh@uwaterloo.ca>
2024-05-28 07:56:05 -0400

    Medium problem

---
+++ src/Find_Users_With_Valid_E-Mails.sql
@@ -0,0 +1,40 @@
+-- 1517. Find Users With Valid E-Mails
+-- SOURCE: https://leetcode.com/problems/find-users-with-valid-e-mails/description/?envType=study-plan-v2&envId=top-sql-50
+
+
+-- PROBLEM:
+-- Table: Users
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | user_id       | int     |
+-- | name          | varchar |
+-- | mail          | varchar |
+-- +---------------+---------+
+-- user_id is the primary key (column with unique values) for this table.
+-- This table contains information of the users signed up in a website. Some e-mails are invalid.
+
+-- Write a solution to find the users who have valid emails.
+-- A valid e-mail has a prefix name and a domain where:
+
+-- The prefix name is a string that may contain letters (upper or lower case), digits, underscore '_', period '.', and/or dash '-'. The prefix name must start with a letter.
+-- The domain is '@leetcode.com'.
+-- Return the result table in any order.
+
+-- TABLE SCHEMA:
+Create table If Not Exists Users (user_id int, name varchar(30), mail varchar(50))
+Truncate table Users
+insert into Users (user_id, name, mail) values ('1', 'Winston', 'winston@leetcode.com')
+insert into Users (user_id, name, mail) values ('2', 'Jonathan', 'jonathanisgreat')
+insert into Users (user_id, name, mail) values ('3', 'Annabelle', 'bella-@leetcode.com')
+insert into Users (user_id, name, mail) values ('4', 'Sally', 'sally.come@leetcode.com')
+insert into Users (user_id, name, mail) values ('5', 'Marwan', 'quarz#2020@leetcode.com')
+insert into Users (user_id, name, mail) values ('6', 'David', 'david69@gmail.com')
+insert into Users (user_id, name, mail) values ('7', 'Shapiro', '.shapo@leetcode.com')
+
+-- SOLUTION:
+SELECT *
+FROM Users
+WHERE mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com$'
+
