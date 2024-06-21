df84049bc5fc6ae07a47bfd9cfa4f9ea35a439ab
Aishwarya-University-of-waterloo <aswagh@uwaterloo.ca>
2024-06-21 15:52:54 -0400

    Medium problem

---
+++ src/Rank_Scores.sql
@@ -0,0 +1,37 @@
+-- 178. Rank Scores
+-- SOURCE: https://leetcode.com/problems/rank-scores/description/
+
+-- PROBLEM STATEMENT:
+-- Table: Scores
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | score       | decimal |
+-- +-------------+---------+
+-- id is the primary key (column with unique values) for this table.
+-- Each row of this table contains the score of a game. Score is a floating point value with two decimal places.
+
+-- Write a solution to find the rank of the scores. The ranking should be calculated according to the following rules:
+
+-- The scores should be ranked from the highest to the lowest.
+-- If there is a tie between two scores, both should have the same ranking.
+-- After a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no holes between ranks.
+-- Return the result table ordered by score in descending order.
+
+-- TABLE SCHEMA:
+Create table If Not Exists Scores (id int, score DECIMAL(3,2))
+Truncate table Scores
+insert into Scores (id, score) values ('1', '3.5')
+insert into Scores (id, score) values ('2', '3.65')
+insert into Scores (id, score) values ('3', '4.0')
+insert into Scores (id, score) values ('4', '3.85')
+insert into Scores (id, score) values ('5', '4.0')
+insert into Scores (id, score) values ('6', '3.65')
+
+-- SOLUTION:
+SELECT
+    score,
+    dense_rank() over(order by score desc) as 'rank'
+FROM scores
