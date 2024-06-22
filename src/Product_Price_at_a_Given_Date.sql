a0718393816f6888f1bb4d60025fdbab3fb3fba2
Aishwarya-University-of-waterloo <aswagh@uwaterloo.ca>
2024-06-21 23:19:10 -0400

    Medium problem

--- src/Product_Price_at_a_Given_Date.sql
+++ src/Product_Price_at_a_Given_Date.sql
@@ -19,8 +19,8 @@
 
 
 -- TABLE SCHEMA:
-Create table If Not Exists Products (product_id int, new_price int, change_date date)
-Truncate table Products
+Create OR replace TABLE Products(product_id int, new_price int, change_date date);
+Truncate table Products;
 insert into Products (product_id, new_price, change_date) values ('1', '20', '2019-08-14')
 insert into Products (product_id, new_price, change_date) values ('2', '50', '2019-08-14')
 insert into Products (product_id, new_price, change_date) values ('1', '30', '2019-08-15')
@@ -30,3 +30,17 @@ insert into Products (product_id, new_price, change_date) values ('3', '20', '20
 
 
 -- SOLUTION:
+
+SELECT
+    distinct product_id,
+    10 as price
+FROM Products
+    where product_id not in( select distinct product_id from Products where change_date <='2019-08-16' )
+
+UNION
+
+SELECT
+    product_id,
+    new_price as price
+FROM Products
+    where (product_id,change_date) in ( select product_id , max(change_date) as date from Products where change_date <='2019-08-16' group by product_id)
