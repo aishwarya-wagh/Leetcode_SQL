4cd62e761362a79fdcf80f0f8705f4c5e0277fe4
Aishwarya-University-of-waterloo <aswagh@uwaterloo.ca>
2024-06-30 12:06:45 -0400

    Medium problem

--- src/Tree_Node.sql
+++ src/Tree_Node.sql
@@ -33,3 +33,12 @@ insert into Tree (id, p_id) values ('5', '2')
 
 -- SOLUTION:
 
+select
+id,
+case
+    when p_id is null then 'Root'
+    when id  in (select p_id from tree) then 'Inner'
+    else 'Leaf'
+end as type
+from tree
+
