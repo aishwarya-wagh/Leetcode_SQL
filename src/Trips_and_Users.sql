ea0de8730b55eb6b65016d1885db0df3201c0909
Aishwarya-University-of-waterloo <aswagh@uwaterloo.ca>
2024-06-30 18:35:36 -0400

    initial commit

---
+++ src/Trips_and_Users.sql
@@ -0,0 +1,64 @@
+-- 262. Trips and Users
+-- SOURCE: https://leetcode.com/problems/trips-and-users/description/
+
+-- PROBLEM STATEMENT:
+-- Table: Trips
+
+-- +-------------+----------+
+-- | Column Name | Type     |
+-- +-------------+----------+
+-- | id          | int      |
+-- | client_id   | int      |
+-- | driver_id   | int      |
+-- | city_id     | int      |
+-- | status      | enum     |
+-- | request_at  | date     |
+-- +-------------+----------+
+-- id is the primary key (column with unique values) for this table.
+-- The table holds all taxi trips. Each trip has a unique id, while client_id and driver_id are foreign keys to the users_id at the Users table.
+-- Status is an ENUM (category) type of ('completed', 'cancelled_by_driver', 'cancelled_by_client').
+
+-- Table: Users
+
+-- +-------------+----------+
+-- | Column Name | Type     |
+-- +-------------+----------+
+-- | users_id    | int      |
+-- | banned      | enum     |
+-- | role        | enum     |
+-- +-------------+----------+
+-- users_id is the primary key (column with unique values) for this table.
+-- The table holds all users. Each user has a unique users_id, and role is an ENUM type of ('client', 'driver', 'partner').
+-- banned is an ENUM (category) type of ('Yes', 'No').
+
+-- The cancellation rate is computed by dividing the number of canceled (by client or driver) requests with unbanned users by the total number of requests with unbanned users on that day.
+-- Write a solution to find the cancellation rate of requests with unbanned users (both client and driver must not be banned) each day between "2013-10-01" and "2013-10-03". Round Cancellation Rate to two decimal points.
+
+-- Return the result table in any order.
+
+
+-- TABLE SCHEMA:
+Create table If Not Exists Trips (id int, client_id int, driver_id int, city_id int, status ENUM('completed', 'cancelled_by_driver', 'cancelled_by_client'), request_at varchar(50))
+Create table If Not Exists Users (users_id int, banned varchar(50), role ENUM('client', 'driver', 'partner'))
+Truncate table Trips
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('1', '1', '10', '1', 'completed', '2013-10-01')
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('2', '2', '11', '1', 'cancelled_by_driver', '2013-10-01')
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('3', '3', '12', '6', 'completed', '2013-10-01')
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('4', '4', '13', '6', 'cancelled_by_client', '2013-10-01')
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('5', '1', '10', '1', 'completed', '2013-10-02')
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('6', '2', '11', '6', 'completed', '2013-10-02')
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('7', '3', '12', '6', 'completed', '2013-10-02')
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('8', '2', '12', '12', 'completed', '2013-10-03')
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('9', '3', '10', '12', 'completed', '2013-10-03')
+insert into Trips (id, client_id, driver_id, city_id, status, request_at) values ('10', '4', '13', '12', 'cancelled_by_driver', '2013-10-03')
+Truncate table Users
+insert into Users (users_id, banned, role) values ('1', 'No', 'client')
+insert into Users (users_id, banned, role) values ('2', 'Yes', 'client')
+insert into Users (users_id, banned, role) values ('3', 'No', 'client')
+insert into Users (users_id, banned, role) values ('4', 'No', 'client')
+insert into Users (users_id, banned, role) values ('10', 'No', 'driver')
+insert into Users (users_id, banned, role) values ('11', 'No', 'driver')
+insert into Users (users_id, banned, role) values ('12', 'No', 'driver')
+insert into Users (users_id, banned, role) values ('13', 'No', 'driver')
+
+-- SOLUTION:
