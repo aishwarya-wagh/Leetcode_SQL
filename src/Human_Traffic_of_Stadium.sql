620e6f70f897d38fa51a4112dc63ffbaefbf692a
Aishwarya-University-of-waterloo <aswagh@uwaterloo.ca>
2024-07-01 18:52:02 -0400

    Hard problem

--- src/Human_Traffic_of_Stadium.sql
+++ src/Human_Traffic_of_Stadium.sql
@@ -34,3 +34,16 @@ insert into Stadium (id, visit_date, people) values ('8', '2017-01-09', '188')
 
 
 -- SOLUTION:
+with q1 as (
+    select *,
+    id - row_number() over() as id_diff
+    from stadium
+    where people > 99
+)
+select
+    id,
+    visit_date,
+    people
+from q1
+    where id_diff in (select id_diff from q1 group by id_diff having count(*) > 2)
+    order by visit_date
